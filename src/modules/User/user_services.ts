f46a163ed72100d6ffb9fe5bc2190ba454053110
Durjoy-dcp <unknownccxv@gmail.com>
2023-11-25 18:53:38 +0600

    without password

--- src/modules/User/user_services.ts
+++ src/modules/User/user_services.ts
@@ -1,9 +1,15 @@
 import { User } from "./user_interface";
 import { UserModel } from "./user_model";
 
-const CreateUserToDB = async (user: User) => {
-  const result = UserModel.create(user);
-  return result;
+const CreateUserToDB = async (userData: User) => {
+  const result = await UserModel.create(userData);
+
+  const returnResult = await UserModel.findOne(
+    { userId: result.userId },
+    { password: 0 }
+  );
+
+  return returnResult;
 };
 export const UserServices = {
   CreateUserToDB,
