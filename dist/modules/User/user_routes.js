e63dd08f8e9a0c8dbc44b43fc2225ed0555fa600
Durjoy-dcp <unknownccxv@gmail.com>
2023-11-29 23:13:30 +0600

    put route

--- dist/modules/User/user_routes.js
+++ dist/modules/User/user_routes.js
@@ -10,7 +10,7 @@ const router = express_1.default.Router();
 router.post("/", user_controller_1.UserController.createUser);
 router.get("/", user_controller_1.UserController.getUsers);
 router.get("/:userId", user_controller_1.UserController.getSingleUser);
-router.post("/:userId", user_controller_1.UserController.editUser);
+router.put("/:userId", user_controller_1.UserController.editUser);
 router.delete("/:userId", user_controller_1.UserController.deleteUser);
 router.put("/:userId/orders", user_controller_1.UserController.addOrder);
 router.get("/:userId/orders", user_controller_1.UserController.getOrder);
--- src/modules/User/user_routes.ts
+++ src/modules/User/user_routes.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post("/", UserController.createUser);
 router.get("/", UserController.getUsers);
 router.get("/:userId", UserController.getSingleUser);
-router.post("/:userId", UserController.editUser);
+router.put("/:userId", UserController.editUser);
 router.delete("/:userId", UserController.deleteUser);
 router.put("/:userId/orders", UserController.addOrder);
 router.get("/:userId/orders", UserController.getOrder);
